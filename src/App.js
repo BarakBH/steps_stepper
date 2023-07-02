2059f65a2e3bdeaae6cbf68e7a4085a78ed45dc1
BarakBH <barakbhamu@gmail.com>
2023-07-02 19:51:51 +0300

    children prop

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const messages = [
   "Learn React ⚛️",
   "useState Concepts 💥",
-  "Final Steps 🧑‍💻",
+  "Children Props 🧑‍💻",
 ];
 
 export default function App() {
@@ -31,15 +31,27 @@ export default function App() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <p className="message">Step {step}: {messages[step - 1]}</p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+          </StepMessage>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
-            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>Next</button>
+            <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious}> <span>👈🏻</span> Previous </Button>
+            <Button bgColor='#7950f2' textColor='#fff' onClick={handleNext}> Next <span>👉🏻</span> </Button>
           </div>
         </div>
       }
     </>
   )
+}
+
+function StepMessage({ step, children }) {
+  return <div className="message">
+    <h3>Step {step}</h3>
+    {children}
+  </div>
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}> {children} </button>
 }
